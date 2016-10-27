18692ee5e5f6bf2836b76123c3f308b820b37e90
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-10-27 23:28:48 +0000

    task_02 complete

---
+++ task_02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module works with the bands dictionary."""
+
+
+from data import BANDS
+
+NIGEL = BANDS['Spinal Tap']['Nigel Tufnel']
+
+BAND_NAMES = BANDS.keys()
+
+
