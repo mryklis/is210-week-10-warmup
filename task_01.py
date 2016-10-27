51edc71a26a61801f46894b99a648d6f9c3963b9
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-10-27 23:06:29 +0000

    task01 complete

---
+++ task_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module creates a dictionary of grades."""
+
+
+GRADE_DATA = {
+    'Luke Skywalker': {'math': 'B', 'etiquette': 'B+', 'grammar': 'B', 'gym': 'A'},
+    'HAn Solo': {'math': 'A-', 'etiquette': 'C-', 'grammar': 'B', 'gym': 'B'},
+    'C-3PO': {'math': 'C', 'etiquette': 'A+', 'grammar': 'A', 'gym': 'F'}
+    }
+print GRADE_DATA
+    
